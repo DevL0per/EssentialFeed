175167ae1d3f3b5b7a4e14461e87a734c928fc0c
Roman <vazhnik.roma@gmail.com>
2022-11-26 11:11:33 +0300

    Add CoreDataFeedStore data data

---
+++ ManagedCache+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ManagedCache+CoreDataClass.swift
+//  EssentialFeed
+//
+//  Created by x.one on 26.11.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedCache)
+public class ManagedCache: NSManagedObject {
+
+}
---
+++ ManagedFeedImage+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ManagedFeedImage+CoreDataClass.swift
+//  EssentialFeed
+//
+//  Created by x.one on 26.11.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedFeedImage)
+public class ManagedFeedImage: NSManagedObject {
+
+}
