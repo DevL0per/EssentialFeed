f1d2dcd58eebdb79ec1b8598a7b282a939e3892d
Roman <vazhnik.roma@gmail.com>
2022-11-28 21:56:53 +0300

    Create EssentialFeedCacheIntegrationTests LocalFeedLoader load delivers no Items on empty cache LocalFeedLoader load delivers items saved on a separate instance

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,73 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by x.one on 28.11.22.
+//
+
+import XCTest
+import EssentialFeed
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+        let exp = expectation(description: "wait for load result")
+        
+        sut.load { result in
+            switch result {
+            case .success(let items):
+                XCTAssertEqual(items, [])
+            case .failure(let error):
+                XCTFail("expected success case with empty array, got \(error) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueItem
+        
+        let saveExp = expectation(description: "wait for save completion")
+        sutToPerformSave.save([feed]) { error in
+            XCTAssertNil(error, "expected to save successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+        
+        let loadExp = expectation(description: "wait for load completion")
+        sutToPerformLoad.load { result in
+            switch result {
+            case .success(let receivedItems):
+                XCTAssertEqual(receivedItems, [feed])
+            case .failure(let error):
+                XCTFail("expected success case with empty array, got \(error) instead")
+            }
+            loadExp.fulfill()
+        }
+        wait(for: [loadExp], timeout: 1.0)
+    }
+    
+    private func makeSUT() -> LocalFeedLoader {
+        let storeURL = testSpecificStoreURL()
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let coreDataStore = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let localFeedLoader = LocalFeedLoader(store: coreDataStore, timestamp: Date.init)
+        trackForMemoryLeaks(localFeedLoader)
+        trackForMemoryLeaks(coreDataStore)
+        return localFeedLoader
+    }
+    
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+
+}
