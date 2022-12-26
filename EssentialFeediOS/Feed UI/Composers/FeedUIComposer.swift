d812201bfebf600c33dcc07265c4bb05b1b8511e
Roman <vazhnik.roma@gmail.com>
2022-12-26 08:29:33 +0300

    Move dispatching logic to MainQueueDispatchDecorator dispatch method, to make this logic reusable

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -35,19 +35,23 @@ private final class MainQueueDispatchDecorator<T> {
     init(decoratee: T) {
         self.decoratee = decoratee
     }
+    
+    func dispatch(completion: @escaping ()->Void) {
+        if Thread.isMainThread {
+            completion()
+        } else {
+            DispatchQueue.main.async {
+                completion()
+            }
+        }
+    }
 }
 
 extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
     
     func load(completion: @escaping (EssentialFeed.FeedLoaderResult) -> Void) {
-        decoratee.load { result in
-            if Thread.isMainThread {
-                completion(result)
-            } else {
-                DispatchQueue.main.async {
-                    completion(result)
-                }
-            }
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
         }
     }
     
