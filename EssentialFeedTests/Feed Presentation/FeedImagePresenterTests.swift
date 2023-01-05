35020accfa35e8c1c9f236c6bb7353a73787c77b
Roman <vazhnik.roma@gmail.com>
2023-01-05 08:55:02 +0300

    Create FeedImagePresenterTests for migrating from MVVM to MVP

---
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCellPresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by x.one on 5.01.23.
+//
+
+import XCTest
+import EssentialFeed
+
+
+
+final class FeedImagePresenterTests: XCTestCase {
+
+}
