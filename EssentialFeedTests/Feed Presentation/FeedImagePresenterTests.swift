f55eb4f36d185e18dc6a35a466f78939fdffda5e
Roman <vazhnik.roma@gmail.com>
2023-01-05 08:58:28 +0300

    FeedImagePresenter does not load image upon creation

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -8,8 +8,31 @@
 import XCTest
 import EssentialFeed
 
-
+final class FeedImagePresenter {
+    
+    init(view: Any) {
+        
+    }
+    
+}
 
 final class FeedImagePresenterTests: XCTestCase {
-
+    
+    func test_init_doesNotLoadImage() {
+        let (_, view) = makeSUT()
+        XCTAssertEqual(view.messages, [])
+    }
+    
+    private func makeSUT(file: StaticString = #file,
+                         line: UInt = #line) -> (FeedImagePresenter, ViewSPY) {
+        let view = ViewSPY()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
+    
+    private class ViewSPY {
+        var messages: [String] = []
+    }
 }
